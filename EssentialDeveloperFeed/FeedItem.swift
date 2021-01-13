42dad1a3c4071db160e839b0b744973b837c6365
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-01-13 15:59:24 -0300

    main Add feed item struct
    -
    
    Signed-off-by: Lívia Vasconcelos <liu.arquiteta@gmail.com>

---
+++ EssentialDeveloperFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialDeveloperFeed
+//
+//  Created by Livia Vasconcelos on 13/01/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
