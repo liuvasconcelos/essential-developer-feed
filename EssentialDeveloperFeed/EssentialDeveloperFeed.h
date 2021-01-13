39635683050df93e538a7a98412c58b6bc0e023e
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-01-13 15:55:42 -0300

    Initial Commit

---
+++ EssentialDeveloperFeed/EssentialDeveloperFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDeveloperFeed.h
+//  EssentialDeveloperFeed
+//
+//  Created by Livia Vasconcelos on 13/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDeveloperFeed.
+FOUNDATION_EXPORT double EssentialDeveloperFeedVersionNumber;
+
+//! Project version string for EssentialDeveloperFeed.
+FOUNDATION_EXPORT const unsigned char EssentialDeveloperFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDeveloperFeed/PublicHeader.h>
+
+
