3b0d0e4451c11c7e6503dba785d78d68ec4efc3f
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-03-07 16:45:22 -0300

    Assert that load will not complete if RemoteFeedLoader instance has bem deallocated.

--- EssentialDeveloperFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialDeveloperFeed/FeedAPI/RemoteFeedLoader.swift
@@ -28,7 +28,9 @@ final class RemoteFeedLoader {
     }
     
     func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { result in
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
             switch result {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, from: response))
