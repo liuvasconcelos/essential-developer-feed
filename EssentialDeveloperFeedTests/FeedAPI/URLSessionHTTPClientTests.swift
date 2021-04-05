c99d26db4c119ebb09a6dc3fe390cd5815e8d8af
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-04-05 08:20:14 -0300

    Refactor test to avoid data race.

--- EssentialDeveloperFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialDeveloperFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -21,17 +21,20 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestWithURL() {
         let url = anyURL()
-        let exp = expectation(description: "Wait for request")
         
+        var receivedRequests = [URLRequest]()
         URLProtocolStub.observeRequests { request in
-            XCTAssertEqual(request.url, url)
-            XCTAssertEqual(request.httpMethod, "GET")
-            exp.fulfill()
+            receivedRequests.append(request)
         }
         
-        makeSUT().get(from: url) { _ in }
+        let exp = expectation(description: "Wait for request completion")
+        makeSUT().get(from: url) { _ in exp.fulfill() }
         
         wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedRequests.count, 1)
+        XCTAssertEqual(receivedRequests.first?.url, url)
+        XCTAssertEqual(receivedRequests.first?.httpMethod, "GET")
     }
     
     func test_getFromURL_failsOnRequestError() {
