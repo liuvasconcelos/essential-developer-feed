fb53aaa56eb3adad282df642cf994bd9be7bfa5b
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-06-19 16:19:33 -0300

    Replace custom 'HTTPClientResult' enum with a nestes type alias over the standard 'Swift.Result'

--- EssentialDeveloperFeedTests/FeedAPI/LoadFeedFromRemoteUseCasesTests.swift
+++ EssentialDeveloperFeedTests/FeedAPI/LoadFeedFromRemoteUseCasesTests.swift
@@ -173,12 +173,12 @@ class LoadFeedFromRemoteUseCasesTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
@@ -191,7 +191,7 @@ class LoadFeedFromRemoteUseCasesTests: XCTestCase {
                       data: Data,
                       at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
 }
