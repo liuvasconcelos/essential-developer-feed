b9d08dea830a871b5db9a579636a0e4b573280a8
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-07-11 17:50:48 -0300

    Move ViewModels to separate files

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Livia Vasconcelos on 11/07/21.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Livia Vasconcelos on 11/07/21.
+//
+
+import EssentialDeveloperFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
