7d35afc04e2859ebbcb222971425a1eb721c6959
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-07-18 17:05:09 -0300

    Move composers to its own files.

---
+++ EssentialFeediOS/FeedUI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,32 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Livia Vasconcelos on 18/07/21.
+//
+
+import Foundation
+import EssentialDeveloperFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
+
