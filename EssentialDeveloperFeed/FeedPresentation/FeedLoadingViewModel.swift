feb7ed46dfb05a0160ffae4c4dbf1cdd35b96ec7
Lívia Vasconcelos <liu.arquiteta@gmail.com>
2021-09-12 18:04:10 -0300

    Separate view model on its own files

---
+++ EssentialDeveloperFeed/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialDeveloperFeed
+//
+//  Created by Livia Vasconcelos on 12/09/21.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialDeveloperFeed/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialDeveloperFeed
+//
+//  Created by Livia Vasconcelos on 12/09/21.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
